37c01db06267932af29500253b43158295183699
Montoya086 <mon21552@uvg.edu.gt>
2023-04-04 14:12:11 -0600

    navigation sample

--- src/pages/nav_sample.js
+++ src/pages/nav_sample.js
@@ -3,15 +3,13 @@ import { useEffect,useState } from "react"
 import { useSearchParams, createSearchParams } from "react-router-dom"
 import { BrowserRouter, Link } from "react-router-dom"
 
-const Home = () => {
+const Registro = () => {
   const[fetchError, setFetchError]=useState(null)
   const[test, setTest]=useState(null)
   const[rol, setRol]=useState(null)
 
   const [searchparams] = useSearchParams();
   const [user_id] = useState(searchparams.get('id'));
-  const [dni, setDni] = useState('');
-  const [patient, setPatient] = useState('');
 
   useEffect(()=>{
     const fetchTest= async ()=>{
@@ -36,26 +34,11 @@ const Home = () => {
     fetchTest()
   },[user_id])
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault();
-
-    const {data,error}=await supabase
-    .rpc('get_info_from_paciente_with_dni',{p_dni:dni})
-    if(error){
-      console.log(error)
-    }
-    if(data){
-      console.log(data)
-      setPatient(data)
-    }
-  }
-
   return (
-    <div className="page home">
+    <div className="page registro">
       <div className="header">
         <nav>
-          <h1>Pagina inicial</h1>
-          {test&&(<h6>Bienvenido {test[0].nombre}</h6>)}
+          <h1>Registo de Médicos</h1>
           <Link to={{pathname:'/home',search: createSearchParams({id: user_id}).toString()}}>Home</Link>
           <Link to="/">Logout</Link>
           {test&&rol&&(
@@ -67,21 +50,13 @@ const Home = () => {
         </nav>
       </div>
       <div className="body">
-        <div className="search">
-          <form onSubmit={handleSubmit}>
-            <h2>Buscar paciente</h2>
-            <input className='search-input' placeholder='DNI' type="text" value={dni} onChange={(e) => setDni(e.target.value)}/>
-            <button className='search-button' type='submit'>Buscar</button>
-          </form>
-        </div>
-        <div id='result'>
-          {patient&&(
-            <p></p>
-          )}
-        </div>
+        <form className="form-registro">
+          <h2 className='login_title'>Registro</h2>
+          <label></label>
+        </form>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Registro
