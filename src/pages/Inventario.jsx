4f8ba641f9dc8fd1580108d4023670a8d77d6fb1
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2023-04-07 12:40:24 -0600

    Registro de insumo 1
    Registro de insumo básico en Inventario.

--- src/pages/Inventario.jsx
+++ src/pages/Inventario.jsx
@@ -4,41 +4,70 @@ import { useSearchParams, createSearchParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 
 const Inventario = () => {
+  const[searchparams] = useSearchParams();
+  const[user_id] = useState(searchparams.get('id'));
+  
   const[fetchError, setFetchError]=useState(null)
-  const[fecthData, setFetchData]=useState(null)
   const[rol, setRol]=useState(null)
-
-  const [searchparams] = useSearchParams();
-  const [user_id] = useState(searchparams.get('id'));
+  
+  const[test, setTest]=useState(null)
+  const[sesion, setSesion]=useState('')
 
   useEffect(()=>{
     const fetchTest= async ()=>{
       const {data,error}=await supabase
-        .rpc('get_medico_using_user_id',{user_id:user_id})
+      .rpc('get_medico_using_user_id',{user_id:user_id})
 
         if(error){
           setFetchError('Could not fetch')
-          setFetchData(null)
+          setTest(null)
           console.log(fetchError)
         }
         if(data){
-          setFetchData(data)
+          setTest(data)
           setFetchError(null)
+          setSesion(data[0].user_mail)
           if(data[0].user_rol==='admin'){
             setRol(true)
           }
         }
 
     }
-
     fetchTest()
   },[user_id,fetchError])
 
+  const[id_ins, setId_ins]=useState('')
+  const[nombre, setNombre]=useState('')
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    const {data,error}=await supabase
+    .rpc('set_insumo',{
+      id_ins:id_ins,
+      nombre:nombre,
+      log_mail:sesion,
+      log_info:"Se ingresó el insumo "+nombre+" con el ID "+id_ins
+    })
+
+    if(error){
+      console.log(error)
+    }else{
+      alert("Ingresado correctamente")
+      setId_ins('')
+      setNombre('')
+    }
+    if(data){
+      //alert("Ingresado correctamente")
+    }
+  
+  }
+
   return (
-    <div className="page">
+    <div className="page registro insumo">
       <div className="header">
       <nav>
-          <h1>Inventario de insumos</h1>
+          <h1>Página principal</h1>
           {/*Public paths*/}
           <Link to={{pathname:'/home',search: createSearchParams({id: user_id}).toString()}}>Home</Link>
           <Link to={{pathname:'/reportes',search: createSearchParams({id: user_id}).toString()}}>Reportes</Link>
@@ -47,7 +76,7 @@ const Inventario = () => {
           <Link to={{pathname:'/inventario',search: createSearchParams({id: user_id}).toString()}}>Inventario</Link>
           <Link to={{pathname:'/act_datos',search: createSearchParams({id: user_id}).toString()}}>Act. de datos</Link>
           {/*Private paths*/}
-          {fecthData&&rol&&(
+          {test&&rol&&(
             <>
               <Link to={{pathname:'/registro',search: createSearchParams({id: user_id}).toString()}}>Registro de médicos</Link>
               <Link to={{pathname:'/logs',search: createSearchParams({id: user_id}).toString()}}>Logs</Link>
@@ -57,7 +86,20 @@ const Inventario = () => {
         </nav>
       </div>
       <div className="body">
-
+        <form className="form-registro" onSubmit={handleSubmit}>
+          <h2 className='login_title'>Registro de insumo</h2>
+          <div className="text-box">
+            <input type="text" required value={id_ins} onChange={(e) => setId_ins(e.target.value)}/>
+            <label>Id Insumo</label>
+          </div>
+          <div className="text-box">
+            <input type="text" required value={nombre} onChange={(e) => setNombre(e.target.value)} />
+            <label>Nombre</label>
+          </div>
+          <div className="submit-button">
+            <button className='btn' type='submit'>Registrar</button>
+          </div>
+        </form>
       </div>
     </div>
   )
