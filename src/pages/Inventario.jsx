bcb787436381dfbfd549a2dca12b8f13ad45fbe0
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2023-04-07 19:08:44 -0600

    Update Inventario.jsx

--- src/pages/Inventario.jsx
+++ src/pages/Inventario.jsx
@@ -9,7 +9,7 @@ const Inventario = () => {
   
   const[fetchError, setFetchError]=useState(null)
   const[rol, setRol]=useState(null)
-  
+  const[estab, setEstab]=useState(null)
   const[test, setTest]=useState(null)
   const[sesion, setSesion]=useState('')
 
@@ -36,8 +36,30 @@ const Inventario = () => {
     fetchTest()
   },[user_id,fetchError])
 
+  //fetch establecimientos
+  useEffect(()=>{
+    const fetchTest= async ()=>{
+      const {data,error}=await supabase
+      .rpc('get_establecimientos')
+
+      if(error){
+        setFetchError('Could not fetch')
+        setEstab(null)
+        console.log(error)
+      }
+      if(data){
+        setEstab(data)
+        setFetchError(null)
+      }
+
+    }
+    fetchTest()
+  },[])
+
   const[id_ins, setId_ins]=useState('')
   const[nombre, setNombre]=useState('')
+  const[estab_ins, setEstab_ins]=useState('')
+  const[cant_ins, setCant_ins]=useState('')
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
@@ -47,13 +69,36 @@ const Inventario = () => {
       id_ins:id_ins,
       nombre:nombre,
       log_mail:sesion,
-      log_info:"Se ingresó el insumo "+nombre+" con el ID "+id_ins
+      log_info:"Se registró el insumo "+nombre+" con el ID "+id_ins
     })
 
     if(error){
       console.log(error)
     }else{
-      alert("Ingresado correctamente")
+      setId_ins('')
+      setNombre('')
+    }
+    if(data){
+      //alert("Ingresado correctamente")
+    }
+  
+  }
+
+  const handleSubmit2 = async (e) =>{
+    e.preventDefault();
+
+    const {data,error}=await supabase
+    .rpc('set_insumo',{
+      id_ins:id_ins,
+      estab_ins:estab_ins,
+      cant_ins:cant_ins,
+      log_mail:sesion,
+      log_info:"Se ingresó el insumo "+id_ins+" al establecimiento "+estab_ins+" con la cantidad "+cant_ins
+    })
+
+    if(error){
+      console.log(error)
+    }else{
       setId_ins('')
       setNombre('')
     }
@@ -87,7 +132,7 @@ const Inventario = () => {
       </div>
       <div className="body">
         <form className="form-registro" onSubmit={handleSubmit}>
-          <h2 className='login_title'>Registro de insumo</h2>
+        <h2 className='login_title'>Registro de insumo</h2>
           <div className="text-box">
             <input type="text" required value={id_ins} onChange={(e) => setId_ins(e.target.value)}/>
             <label>Id Insumo</label>
@@ -95,6 +140,37 @@ const Inventario = () => {
           <div className="text-box">
             <input type="text" required value={nombre} onChange={(e) => setNombre(e.target.value)} />
             <label>Nombre</label>
+          </div>
+          <div className="submit-button">
+            <button className='btn' type='submit'>Registrar</button>
+          </div>
+        </form>
+        <br></br>
+        <form className="form-registro" onSubmit={handleSubmit2}>
+        <h2 className='login_title'>Establecimiento</h2>
+          <div className="select-box">
+            <select name="Establecimientos" required value={estab_ins} onChange={(e) => setEstab_ins(e.target.value)}>
+              {estab&&(
+                <>
+                  {estab.map(e=>(
+                    <>
+                      <option value="">Seleccione un Establecimiento</option>
+                      <option value={e.id}>{e.estab+", "+e.direccion+", "+e.departamento+", "+e.municipio}</option>
+                    </>
+                  ))}
+                </>
+              )}
+            </select>
+          </div>
+          <br></br>
+          <h2 className='login_title'>Ingreso de insumo</h2>
+          <div className="text-box">
+            <input type="text" required value={id_ins} onChange={(e) => setId_ins(e.target.value)}/>
+            <label>Id Insumo</label>
+          </div>
+          <div className="text-box">
+            <input type="text" required value={cant_ins} onChange={(e) => setCant_ins(e.target.value)} />
+            <label>Cantidad</label>
           </div>
           <div className="submit-button">
             <button className='btn' type='submit'>Registrar</button>
